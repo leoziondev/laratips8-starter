60ed0996a10a64a630f11ba5fc3b55b7963f5857
LeoZion <leozionsuporte@gmail.com>
2021-09-14 04:54:21 -0300

    Routing views and controller pt-01

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,33 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutUsController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+//Route::get('/', function () {
+////    return "Hello World";
+//    return view('welcome');
+//});
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [HomeController::class, 'index']);
+Route::get('about-us', [AboutUsController::class, 'index']);
+
+Route::view('direct-view', 'welcome');
+
+Route::get('another', function() {
+    return 'from another route';
+});
+
+Route::get('user/{username?}', function($username = null) {
+    return "Hello " . $username;
+});
+
+Route::redirect('foo', 'bar');
+
+Route::get('bar', function() {
+    return "Redirect route foo to bar";
+});
+
+Route::fallback(function() {
+    return "fallback section";
 });
